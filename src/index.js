d3b385dd0ecf9c75515926a2a467f440e0b4e9e5
Milen <millalen.pg@gmail.com>
2024-02-27 18:18:22 -0300

    Avance js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import validator from './validator.js';
 
+/* invocar validator.isValid() y validator.maskify() según sea necesario y actualizar el resultado en la UI (interfaz de usuario).*/
+const cardNumber= document.getElementById("cardNumber") // falta agregar eventListener del input
 const btnValidation= document.getElementById("btnValidation")
-btnValidation.addEventListener("click", ()=> {
 
-  validator.validate("123456")
+btnValidation.addEventListener("click", ()=> {
+  validator.isValid(cardNumber)
 })
--- src/validator.js
+++ src/validator.js
@@ -1,8 +1,19 @@
-const validate = (creditNumber) => {
-  console.log(creditNumber)
-}
 const validator = {
-  validate
+
+  /*validator.isValid(creditCardNumber): creditCardNumber es un string con el número de tarjeta que se va a verificar. 
+  Esta función debe retornar un boolean dependiendo si es válida de acuerdo al algoritmo de Luhn.*/
+  isValid: (inputCardNumber) => {
+    console.log(inputCardNumber)
+  },
+
+  /*validator.maskify(creditCardNumber): creditCardNumber es un string con el número de tarjeta y esta función debe 
+  retornar un string donde todos menos los últimos cuatro caracteres sean reemplazados por un numeral (#) o 🐱. 
+  Esta función deberá siempre mantener los últimos cuatro caracteres intactos, aún cuando el string sea de menor longitud. */
+  maskify: (inputCardNumber) => {
+    console.log(inputCardNumber)
+
+  }
+
 };
 
 export default validator;
