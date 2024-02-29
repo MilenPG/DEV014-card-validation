a495171da3e9bc596f37d02ae911422ca4b73b3f
Milen <millalen.pg@gmail.com>
2024-02-29 01:20:48 -0300

    Pseudocódigo js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
 import validator from './validator.js';
 
-/* invocar validator.isValid() y validator.maskify() según sea necesario y actualizar el resultado en la UI (interfaz de usuario).*/
-const cardNumber= document.getElementById("cardNumber") // falta agregar eventListener del input
-const btnValidation= document.getElementById("btnValidation")
+//"Invocar validator.isValid() y validator.maskify() según sea necesario y actualizar el resultado en la UI (interfaz de usuario)."
 
+//Invocar el input de n° de tarjeta, para poder luego pasarle como argumento de las funciones:
+const cardNumber= document.getElementById("cardNumber") // falta agregar eventListener del input
+
+//Invocar las funciones, pasándole cardNumber:
+
+//Llamar al botón:
+const btnValidation= document.getElementById("btnValidation")
+//Hacer que botón funcione:
+//1. Que al escuchar el "click", el botón ejecute... 
 btnValidation.addEventListener("click", ()=> {
-  validator.isValid(cardNumber)
+  //2. if...> innerHTML: "¡Tu tarjeta y suscripción asociada ha sido validada exitosamente!"
+  validator.isValid(cardNumber) // hacer if, invocando a validator
+
+  //3. else...> innerHTML: "Tarjeta inválida. Por favor,chequea e intentalo otra vez.
+  
 })
