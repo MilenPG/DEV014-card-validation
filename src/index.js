7a1b194b044e565e4ac05c2b1b3084a3f6f4112c
Milen <millalen.pg@gmail.com>
2024-03-01 16:18:09 -0300

    Desarrollo algoritmo isValid. Falta hacer las sumas respectivas y el boolean de retorno final

--- src/index.js
+++ src/index.js
@@ -1,20 +1,18 @@
 import validator from './validator.js';
 
-//"Invocar validator.isValid() y validator.maskify() según sea necesario y actualizar el resultado en la UI (interfaz de usuario)."
+const cardNumber= document.getElementById("cardNumber")// ¿aquí debería aplicar el MASKIFY?
+const btnValidation= document.getElementById("btnValidation")
 
 //Invocar el input de n° de tarjeta, para poder luego pasarle como argumento de las funciones:
-const cardNumber= document.getElementById("cardNumber") // falta agregar eventListener del input
+document.getElementById("cardNumber").addEventListener("input", function(){
+  console.log(cardNumber.value)
+})
 
-//Invocar las funciones, pasándole cardNumber:
-
-//Llamar al botón:
-const btnValidation= document.getElementById("btnValidation")
-//Hacer que botón funcione:
-//1. Que al escuchar el "click", el botón ejecute... 
+//1. Que al escuchar el "click", el botón ejecute isValid... 
 btnValidation.addEventListener("click", ()=> {
   //2. if...> innerHTML: "¡Tu tarjeta y suscripción asociada ha sido validada exitosamente!"
-  validator.isValid(cardNumber) // hacer if, invocando a validator
+  validator.isValid(cardNumber.value) // hacer if, invocando a validator que debe retornar TRUE o FALSE.
 
   //3. else...> innerHTML: "Tarjeta inválida. Por favor,chequea e intentalo otra vez.
-  
-})
+
+}) 
--- src/validator.js
+++ src/validator.js
@@ -3,7 +3,43 @@ const validator = {
   /*validator.isValid(creditCardNumber): creditCardNumber es un string con el número de tarjeta que se va a verificar. 
   Esta función debe retornar un boolean dependiendo si es válida de acuerdo al algoritmo de Luhn.*/
   isValid: (inputCardNumber) => {
-    console.log(inputCardNumber)
+    const luhnArr=[]
+    //const byTwo= []
+    const cardNumber = inputCardNumber.split('').map(num=> Number(num)) // 1. Transformar string a array
+    for (let i=cardNumber.length-1; i>=0; i--) { //2. Recorrer array inversamente
+      if (cardNumber[i] % 2 ===0) { //3. Si la posición es par...
+        if (cardNumber[i]*2>10) { //4. Multiplicar valor de índice*2 y si el resultado es >10, SUMAR DIGITOS Y LUEGO PUSH AL luhnArr
+          console.log(cardNumber[i]*2+" sumar digitos y pushear a luhnArr")
+        } else {
+          luhnArr.push(cardNumber[i]*2) //5. Multiplicar valor de índice*2 y si el resultado es < 10, PUSH AL luhnArr
+        }
+      } else {
+        luhnArr.push(cardNumber[i])
+      } // else if (campo vacío) {throw Error} ?????
+      console.log("Array para suma final "+luhnArr)
+
+    }
+  
+
+    /*
+    1. Crear 3 array vacíos: 1. para numero inverso 2. arr para 3. suma final? 
+    2. Recorrer array inversamente y pushearlos al 1 arr creado
+    3. Recorrer nuevo array y crear bucle que en cada pasada 
+        a.if (inv[i] no es par) {push(inv[i]).luhnArr}
+          else (inv[i] es par) {
+            let byTwo=[] 
+            push(inv[i]*10).byTwo {
+              for (){
+                if (byTen[i]<10) {pushear a luhnArr}
+                else {sumar digitos por elto. y pushear la suma a luhnArr -let sumByTen=0; / let splitByTen = byTen[i].split('') / sumByTen+=splitByTen[i]-}
+              }
+            }
+          }
+          ciclo for para suma final (luhnSum+= luhnArr[i])
+          if (luhnSum....) {return valid}
+          else {return invalid}*/
+
+    console.log("Verificando: "+cardNumber)
   },
 
   /*validator.maskify(creditCardNumber): creditCardNumber es un string con el número de tarjeta y esta función debe 
